ddc5a50995cbb4f5738ecb4b771da2e8e7277b06
nearjay06 <nearjoan2018@gmail.com>
2019-08-07 14:49:57 +0300

    todo app assignment

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+const  fullNames = document.querySelector("#fullNames");
+const  submit = document.querySelector(".subBtn");
+
+function createNames(){
+
+    if (value.length<4)
+
+    alert('Name should not be less than 4 characters')
+
+    else(
+        createNames.push("#fullNames")
+
+    );
+
+
+
+
+
+};
+
+console.log();
